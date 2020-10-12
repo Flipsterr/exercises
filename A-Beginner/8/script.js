0b6377b69fa8edf47f201366da207c47706c42be
unknown <filip.von.bromssen@gmail.comm>
2020-10-12 14:56:11 +0200

    Uppgift 8

---
+++ A-Beginner/8/script.js
@@ -0,0 +1,11 @@
+//Detta 
+const meProperties = {
+    firstName: "Filip",
+    lastName: "von Brömssen",
+    age: 17
+}
+console.log(meProperties)
+//Från och med 30 oktober:
+meProperties.age = 18
+
+console.log(meProperties)
